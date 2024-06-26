6eee2081fa2f672c69b8e57363cf6a1201806922
cdance <cdance@provincianet.com.ar>
2024-06-26 10:36:29 -0300

    0002-add-new-structure

---
+++ init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE test;
+CREATE TABLE test.DAYOFFS (
+	name_day varchar(100) NULL,
+	fecha varchar(100) NULL,
+	tipo varchar(100) NULL,
+	descripcion varchar(100) NULL
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=latin1
+COLLATE=latin1_swedish_ci;
